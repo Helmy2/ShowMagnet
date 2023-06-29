e871b50661fddc9e06819cb3355ec3cfe174d57c
helmy <mohelmy.engineer@gmail.com>
2023-06-29 12:16:16 +0300

    Implement test for onboarding screen

---
+++ app/src/androidTest/java/com/example/showmagnet/ui/onboarding/OnboardingScreenTest.kt
@@ -0,0 +1,50 @@
+package com.example.showmagnet.ui.onboarding
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.example.showmagnet.ui.theme.ShowMagnetTheme
+import org.junit.Rule
+import org.junit.Test
+
+class OnboardingScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun firstOnboardingScreen() {
+        composeTestRule.setContent {
+            ShowMagnetTheme {
+                OnboardingScreen(
+                    onEndOnboarding = {}
+                )
+            }
+        }
+
+        val item = OnBoardingItems.getData().first()
+        composeTestRule.onNodeWithText(item.title).assertIsDisplayed()
+        composeTestRule.onNodeWithText(item.subTitle).assertIsDisplayed()
+        composeTestRule.onNodeWithText("Next").assertIsDisplayed()
+    }
+
+    @Test
+    fun onNextButtonNavigateThePager() {
+        var wasCalled = false
+        composeTestRule.setContent {
+            ShowMagnetTheme { OnboardingScreen(onEndOnboarding = { wasCalled = true }) }
+        }
+
+        val items = OnBoardingItems.getData()
+
+        for ((i, item) in items.withIndex()) {
+            composeTestRule.onNodeWithText(item.title).assertIsDisplayed()
+            composeTestRule.onNodeWithText(item.subTitle).assertIsDisplayed()
+
+            val buttonText = if (i == items.lastIndex) "Start" else "Next"
+            composeTestRule.onNodeWithText(buttonText).assertIsDisplayed()
+            composeTestRule.onNodeWithText(buttonText).performClick()
+        }
+        assert(wasCalled)
+    }
+}
